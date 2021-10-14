717573c3b23b32db8626f648952efb2c4b30b76c
veekshith14 <kalamula14@gmail.com>
2021-10-14 22:58:17 +0530

    dynamic num to corner

--- zerotocorner.js
+++ zerotocorner.js
@@ -1,10 +1,11 @@
-var arr=[1,2,3,4,5,0,0,0,0,5,8,1,5,4,];
+//sending zeros to a corner
+//var arr=[1,2,3,4,5,0,0,0,0,5,8,1,5,4,];
 var newArr1=[];
 var newArr2=[]
-var elem=0;
-function zero(){
+//var elem=0;
+function zero(arr,elem){
 for(var i=0;i<arr.length;i++){
-    if(arr[i]!=0){
+    if(arr[i]!=elem){
         newArr1.push(arr[i]);
         
     }else{
@@ -16,10 +17,10 @@ for(var i=0;i<arr.length;i++){
 }
 var times=newArr2.length;
 for( var j =0; j< times+1; j++){
-    newArr1.push(0);
+    newArr1.push(elem);
 }
 
 return newArr1 ;
 }
-var result=zero();
+var result=zero([5,2,0,5,5,5,5,5,6,0,7,8,9,0,0,5],5);
 console.log(result);
